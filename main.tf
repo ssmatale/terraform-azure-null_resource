6395b9e7e0387603e2fa618fc839c0287ccc69a6
SMatale <smatale@slb.com>
2020-01-21 14:57:30 +0530

    version 2 deployment

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "null_resource" "cluster" {
     command = "echo this_is_version_1"
   }
 }
+
+resource "null_resource" "cluster02" {
+  provisioner "local-exec" {
+      # Bootstrap script called with private_ip of each node in the clutser
+    command = "echo this_is_version_2"
+  }
+}
